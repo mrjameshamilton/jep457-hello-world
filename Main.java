1987f534576f91ca5613aca3ef093de86f20d0f5
james <mrjameshamilton@gmail.com>
2023-10-07 10:24:41 +0200

    Remove withVersion line

--- Main.java
+++ Main.java
@@ -10,7 +10,6 @@ import static jdk.internal.classfile.Classfile.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         Classfile.of().buildTo(Path.of("HelloWorld.class"), ClassDesc.of("HelloWorld"), classBuilder -> classBuilder
-          .withVersion(JAVA_6_VERSION, 0)
           .withMethod("main", ofDescriptor("([Ljava/lang/String;)V"), ACC_PUBLIC | ACC_STATIC, methodBuilder -> methodBuilder.withCode(codeBuilder -> codeBuilder
             .getstatic(of("java.lang.System"), "out", of("java.io.PrintStream"))
             .ldc("Hello World")
